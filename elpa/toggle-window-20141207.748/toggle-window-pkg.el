d8541f9dc484385d1ea92a6c4a28f2933f7b8f5a
Emil Tomov <bemcho@gmail.com>
2017-03-25 15:46:23 +0200

    Split buffer and sr-speedbar key maps

---
+++ elpa/toggle-window-20141207.748/toggle-window-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "toggle-window" "20141207.748" "toggle current window size between half and full" 'nil :url "https://github.com/deadghost/toggle-window" :keywords '("hide" "window"))
